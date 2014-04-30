8d6dd97c078867d5b69a4f19c063fb422b32f304
Alex Pelletier <alexpell00@yahoo.com>
2014-04-30 08:16:55 -0600

    socket class and read me up dates

---
+++ bot.py
@@ -0,0 +1,35 @@
+import socket
+
+class mysocket:
+    '''demonstration class only
+      - coded for clarity, not efficiency
+    '''
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.sendall(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        msg = ''
+        while len(msg) < MSGLEN:
+            chunk = self.sock.recv(MSGLEN-len(msg))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            msg = msg + chunk
+        return msg
+
+
